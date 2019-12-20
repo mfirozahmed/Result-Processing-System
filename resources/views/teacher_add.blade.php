db67deb84360a275a5253b7eb3f0a5aca86dbe5c
MushtaqShahriyarRafee <44242564+MushtaqShahriyarRafee@users.noreply.github.com>
2019-12-21 00:33:33 +0600

    updated

--- resources/views/teacher_add.blade.php
+++ resources/views/teacher_add.blade.php
@@ -697,22 +697,19 @@
 
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
-            <form>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Enter Username(To Add)</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                </div>
-                <!-- <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div> -->
-                <!-- <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div> -->
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+            <br>
+            <br>
+            <div class="container center_div">
+                <form style="width: 300px;">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Enter Username(To Add)</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+
+                    <br>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
         </div>
         <!-- /.content-wrapper -->
         <footer class="main-footer">
