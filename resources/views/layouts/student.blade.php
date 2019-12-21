5fd8b5cee2c834e3bad5979ced13a4c260b0c053
MushtaqShahriyarRafee <44242564+MushtaqShahriyarRafee@users.noreply.github.com>
2019-12-21 23:57:17 +0600

    updated

--- resources/views/layouts/student.blade.php
+++ resources/views/layouts/student.blade.php
@@ -115,7 +115,7 @@
                         <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
-                        <a>{{ Auth::user()->name }}</a>
+                        <a>{{ Auth::user()->username }}</a>
                     </div>
                 </div>
 
--- resources/views/layouts/teacher.blade.php
+++ resources/views/layouts/teacher.blade.php
@@ -116,7 +116,7 @@
                         <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+                        <a href="#" class="d-block">{{ Auth::user()->username }}</a>
                     </div>
                 </div>
 
