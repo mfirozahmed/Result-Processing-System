7d2799adca6e223ef34b35570b938615dd998994
MushtaqShahriyarRafee <44242564+MushtaqShahriyarRafee@users.noreply.github.com>
2019-12-24 13:27:16 +0600

    updated

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -29,7 +29,6 @@
                 <div class="small-box bg-info">
                     <div class="inner">
                         <h3>150</h3>
-
                         <p>New Orders</p>
                     </div>
                     <div class="icon">
@@ -38,29 +37,25 @@
                     <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
-            <!-- ./col -->
+           
             <div class="col-lg-3 col-6">
-                <!-- small box -->
-                <div class="small-box bg-success">
+                <div class="small-box bg-warning">
                     <div class="inner">
-                        <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-                        <p>Bounce Rate</p>
+                        <h3>44</h3>
+                        <p>Total Students</p>
                     </div>
                     <div class="icon">
-                        <i class="ion ion-stats-bars"></i>
+                        <i class="ion ion-person-add"></i>
                     </div>
                     <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
-            <!-- ./col -->
+
             <div class="col-lg-3 col-6">
-                <!-- small box -->
                 <div class="small-box bg-warning">
                     <div class="inner">
                         <h3>44</h3>
-
-                        <p>User Registrations</p>
+                        <p>Total Teachers</p>
                     </div>
                     <div class="icon">
                         <i class="ion ion-person-add"></i>
@@ -70,7 +65,6 @@
             </div>
             <!-- ./col -->
             <div class="col-lg-3 col-6">
-                <!-- small box -->
                 <div class="small-box bg-danger">
                     <div class="inner">
                         <h3>65</h3>
