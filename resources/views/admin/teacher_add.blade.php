e08da54f047afc7d6931118d6e0de606a38c750a
rough22 <mfiroz22@student.sust.edu>
2020-01-12 13:42:07 +0600

    Revert "updated"
    This reverts commit 361f398ab1e7f1ac97c2c938421e9c3ff9f3d837.
    
    	modified:   resources/views/admin/teacher_add.blade.php
    	modified:   resources/views/admin/teacher_remove.blade.php

--- resources/views/admin/teacher_add.blade.php
+++ resources/views/admin/teacher_add.blade.php
@@ -103,7 +103,7 @@
                         <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">{{ Auth::user()->username }}</a>
+                        <a href="#" class="d-block">{{ Auth::user()->name }}</a>
                     </div>
                 </div>
 
